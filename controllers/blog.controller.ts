6ecb209f577c9620b615bab9c111a9db981343f4
kenarigame <kenarigame@gmail.com>
2026-07-06 21:20:17 +0700

    feat: add create blog

--- controllers/blog.controller.ts
+++ controllers/blog.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import {
+  createBlogService,
   getBlogBySlugService,
   getBlogsService,
 } from "../services/blog.service.js";
@@ -14,5 +15,11 @@ export const getBlogsController = async (req: Request, res: Response) => {
 export const getBlogBySlugController = async (req: Request, res: Response) => {
   const slug = req.params.slug as string;
   const result = await getBlogBySlugService(slug);
+  res.status(200).send(result);
+};
+
+export const createBlogController = async (req: Request, res: Response) => {
+  const userId = res.locals.user.id;
+  const result = await createBlogService(req.body, userId);
   res.status(200).send(result);
 };
--- routes/blog.routes.ts
+++ routes/blog.routes.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import {
+  createBlogController,
   getBlogBySlugController,
   getBlogsController,
 } from "../controllers/blog.controller.js";
+import { verifyToken } from "../middlewares/auth.middleware.js";
 
 const blogRoutes = express.Router();
 
 blogRoutes.get("/", getBlogsController);
 blogRoutes.get("/:slug", getBlogBySlugController);
+blogRoutes.post("/", verifyToken(process.env.JWT_SECRET!) ,createBlogController);
 
 export { blogRoutes };
